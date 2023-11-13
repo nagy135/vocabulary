53758c06915e6e2961fce415bfe7f71b8c7af602
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-11-13 17:08:53 +0100

    fix: clerk version change

--- src/app/practice/page.tsx
+++ src/app/practice/page.tsx
@@ -3,18 +3,18 @@ import Navigation from "../_components/navigation";
 import { Practice } from "../_components/practice";
 import { api } from "~/trpc/server";
 
-export default function PracticePage() {
-  // const practicePairs = (await api.word.getAllByUserId.query()).map((e) => {
-  //   return {
-  //     name: e.name ?? "",
-  //     translation: e.translation ?? "",
-  //   };
-  // });
+export default async function PracticePage() {
+  const practicePairs = (await api.word.getAllByUserId.query()).map((e) => {
+    return {
+      name: e.name ?? "",
+      translation: e.translation ?? "",
+    };
+  });
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center ">
       <Navigation currentPage={Page.practice} />
-      <Practice practicePairs={[]} />
+      <Practice practicePairs={practicePairs} />
     </main>
   );
 }
