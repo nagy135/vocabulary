f7690550be5e10fa2033f6a21d7c3f1e0ae99a65
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-11-13 16:50:25 +0100

    fix: remove trpc query

--- src/app/practice/page.tsx
+++ src/app/practice/page.tsx
@@ -4,17 +4,17 @@ import { Practice } from "../_components/practice";
 import { api } from "~/trpc/server";
 
 export default async function PracticePage() {
-  const practicePairs = (await api.word.getAllByUserId.query()).map((e) => {
-    return {
-      name: e.name ?? "",
-      translation: e.translation ?? "",
-    };
-  });
+  // const practicePairs = (await api.word.getAllByUserId.query()).map((e) => {
+  //   return {
+  //     name: e.name ?? "",
+  //     translation: e.translation ?? "",
+  //   };
+  // });
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center ">
       <Navigation currentPage={Page.practice} />
-      <Practice practicePairs={practicePairs} />
+      <Practice practicePairs={[]} />
     </main>
   );
 }
