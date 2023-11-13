1b7cd801e3c6a27d2e43e37ea8ce52458f888fc1
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-11-13 16:57:02 +0100

    fix: lint

--- src/app/practice/page.tsx
+++ src/app/practice/page.tsx
@@ -3,7 +3,7 @@ import Navigation from "../_components/navigation";
 import { Practice } from "../_components/practice";
 import { api } from "~/trpc/server";
 
-export default async function PracticePage() {
+export default function PracticePage() {
   // const practicePairs = (await api.word.getAllByUserId.query()).map((e) => {
   //   return {
   //     name: e.name ?? "",
