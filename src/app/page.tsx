19d618848ca42ce72a47914012e6538932057d75
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-11-27 13:20:16 +0100

    feat: logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { CreateWord } from "~/app/_components/create-word";
 import { Page } from "~/enums";
 import Title from "./_components/title";
+import Logo from "./_components/logo";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center ">
+      <Logo />
       <Title title={"Vocabulary"} page={Page.home} />
       <CreateWord />
     </main>
